0739aa35da0ab587725b6dde15385b27c227f3eb
Schmitz <Schmitz.Sebastian@bcg.com>
2020-01-01 23:05:14 +0100

    completed timesheet

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
-let header = document.createElement("h1");
+// $(function() {
+// 	function labelfocusout () {
+// 		alert("hih");
+// 	}
+// });
 
-header.innerHTML = "Hello World!";
-header.classList.add("my_header");
 
-document.body.appendChild(header);
+$(document).on("focusout", "textarea", function(){
+
+	if ($(this).val() == "") {
+		$(this).val("Bitte Inhalte des Coachings kurz und knapp beschreiben.");
+	}
+})
+
+$(document).on("focusin", "textarea", function(){
+
+	if ($(this).val() == "Bitte Inhalte des Coachings kurz und knapp beschreiben.") {
+		$(this).val("");
+	}
+
+	// if (($(this).val() === "") && ($(this).next().hasClass("normalinput"))) {
+	// 	$(this).next().css({"top": "-75px",
+	// 					"font-size": "1.5em",
+	// 					"transition": "all .2s",
+	// 					"font-style": "italic",
+	// 					"color": "black"});
+	// }
+})
 
